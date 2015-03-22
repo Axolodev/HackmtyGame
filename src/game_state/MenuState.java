4bf76fe91e121bad295d5e20e2f80689fdf0a060
mortaldraw <robildruirey@hotmail.com>
2015-03-22 05:13:54 -0600

    sdfghj

--- src/game_state/MenuState.java
+++ src/game_state/MenuState.java
@@ -13,7 +13,7 @@ public class MenuState extends GameState {
 
 	private Background bg;
 
-	private int currentChoice = 0;
+	private int currentChoice = 1;
 	private String[] options = { "Iniciar", "Ayuda", "Puntuaciones", "Salir" };
 	private Boton flechaDerecha, flechaIzquierda;
 
@@ -21,10 +21,15 @@ public class MenuState extends GameState {
 	private Font titleFont;
 
 	private Font font;
+	private int gameButtonY;
 
-	private Boton gameOne;
-	private Boton gameTwo;
+	private boolean movingRight = false;
+	private boolean movingLeft = false;
+	private Boton[] botonesDeJuego;
+	private int NUMEROBOTONES = 3;
 
+	private int movementCounter;
+	
 	public MenuState(GameStateManager gsm) {
 		this.gsm = gsm;
 		try {
@@ -42,23 +47,15 @@ public class MenuState extends GameState {
 	}
 
 	public void init() {
-		gameOne = new Boton();
-		gameTwo = new Boton();
+		botonesDeJuego = new Boton[NUMEROBOTONES];
+		gameButtonY = GamePanel.HEIGHT / 2;
 		flechaDerecha = new Boton();
 		flechaIzquierda = new Boton();
-		gameOne.setWidth(200);
-		gameOne.setHeight(200);
-		gameTwo.setWidth(200);
-		gameTwo.setHeight(200);
+
 		flechaDerecha.setWidth(140);
 		flechaDerecha.setHeight(60);
 		flechaIzquierda.setWidth(140);
 		flechaIzquierda.setHeight(60);
-
-		gameOne.loadImagesFromStringWithExtension(
-				"/Backgrounds/Botones/placeholderImage", 1, ".png");
-		gameTwo.loadImagesFromStringWithExtension(
-				"/Backgrounds/Botones/placeholderImage", 1, ".png");
 		flechaDerecha.loadImagesFromStringWithExtension(
 				"/Backgrounds/Entidades/Flecha/", 9, ".png");
 		flechaIzquierda.loadImagesFromStringWithExtension(
@@ -66,18 +63,28 @@ public class MenuState extends GameState {
 		flechaDerecha.setFacingRight(true);
 		flechaDerecha.setPosition(GamePanel.WIDTH - 50,
 				GamePanel.HEIGHT / 2 - 46);
-		flechaIzquierda.setPosition(50, GamePanel.HEIGHT / 2 + 20);
-		gameOne.setPosition(GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2 );
-		gameTwo.setPosition(GamePanel.WIDTH / 2, GamePanel.HEIGHT / 2 );
-		System.out.println(gameOne.getx() + ":" + gameOne.gety());
+		flechaIzquierda.setPosition(50, gameButtonY + 20);
 		flechaDerecha.setAnimationDelay(70);
 		flechaIzquierda.setAnimationDelay(70);
+
+		for (int i = 0; i < NUMEROBOTONES; i++) {
+			botonesDeJuego[i].setWidth(200);
+			botonesDeJuego[i].setHeight(200);
+			botonesDeJuego[i].loadImagesFromStringWithExtension(
+					"/Backgrounds/Botones/placeholderImage", 1, ".png");
+			botonesDeJuego[i].setPosition(-125 + 525 * i, gameButtonY);
+		}
+
 	}
 
 	public void update() {
 		bg.update();
 		flechaDerecha.update();
 		flechaIzquierda.update();
+		movementCounter++;
+		for (int i = 0; i < NUMEROBOTONES; i++) {
+			botonesDeJuego[i].update();
+		}
 	}
 
 	/**
@@ -106,8 +113,9 @@ public class MenuState extends GameState {
 			}
 			g.drawString(options[i], 145, 140 + i * 40);
 		}
-		gameOne.draw(g);
-		gameTwo.draw(g);
+		for(int i = 0 ; i < NUMEROBOTONES ; i++){
+			botonesDeJuego[i].setVector(-6, 0);
+		}
 		flechaDerecha.draw(g);
 		flechaIzquierda.draw(g);
 	}
@@ -129,15 +137,23 @@ public class MenuState extends GameState {
 			select();
 		}
 		if (k == KeyEvent.VK_LEFT) {
+			movementCounter = 0;
 			currentChoice--;
 			if (currentChoice == -1) {
 				currentChoice = options.length - 1;
 			}
+			for(int i = 0 ; i < NUMEROBOTONES ; i++){
+				botonesDeJuego[i].setVector(-6, 0);
+			}
 		}
 		if (k == KeyEvent.VK_RIGHT) {
+			movementCounter = 0;
 			currentChoice++;
 			if (currentChoice == options.length) {
 				currentChoice = 0;
+			}
+			for(int i = 0 ; i < NUMEROBOTONES ; i++){
+				botonesDeJuego[i].setVector(-6, 0);
 			}
 		}
 		
