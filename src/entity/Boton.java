32243213ae8057ec494fd764b2d4aacfe32d973d
mortaldraw <robildruirey@hotmail.com>
2015-03-22 00:38:18 -0600

    Agregar clase boton

--- src/entity/Boton.java
+++ src/entity/Boton.java
@@ -1,5 +1,20 @@
 package entity;
 
-public class Boton {
+import java.awt.Graphics2D;
 
+public class Boton extends ScreenObject{
+	
+	public Boton(){
+		this.width = 50;
+		this.height = 50;
+		this.setPosition(400,200);
+		loadImagesFromStringWithExtension("/Backgrounds/Entidades/Triangulo", 1,".png");
+
+	}
+	
+	public void draw(Graphics2D g){
+		setMapPosition();
+		super.draw(g);
+		
+	}
 }
