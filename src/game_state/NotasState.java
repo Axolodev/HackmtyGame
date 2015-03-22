d4db8de85d9e62b39a94ec50f73d7d2eae886734
eGuajardoMozo <estefania_gm13@hotmail.com>
2015-03-22 07:18:13 -0600

    Merge branch 'master' of https://github.com/mortaldraw/HackmtyGame
    Conflicts:
    	src/game_state/NotasState.java

--- src/game_state/NotasState.java
+++ src/game_state/NotasState.java
@@ -9,10 +9,6 @@ import entity.Notas;
 public class NotasState extends GameState {
 
 	private Background bg;
-<<<<<<< HEAD
-
-=======
->>>>>>> 8d7e84b2b17d98f9ef8a105910d554e0f1ebbdfc
 	private Notas[] nota;
 	private boolean siguiente;
 	private int contMax;
